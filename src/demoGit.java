c6307a72ade431a173e01db149794679684693b0
Aishwarya <aishwaryapatil741@gmail.com>
2021-01-29 15:56:27 +0530

    first commit

---
+++ src/demoGit.java
@@ -0,0 +1,15 @@
+public class demoGit {
+    public static void main(String [] args){
+
+        String hungry="Yes";
+
+        if(hungry == "yes")
+            System.out.println("eat something......");
+        else {
+            System.out.println("Do Work.....");
+        }
+
+
+    }
+
+}
