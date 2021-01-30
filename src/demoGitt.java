f09c75175b2ecb6026876620bac6bed2847e2a13
Aishwarya <aishwaryapatil741@gmail.com>
2021-01-30 14:15:30 +0530

    Second commit

---
+++ src/demoGitt.java
@@ -0,0 +1,15 @@
+public class demoGitt {
+    public static void main(String [] args){
+
+        String hungry="Yes";
+
+        if(hungry == "yes")
+            System.out.println("eat something......");
+        else {
+            System.out.println("Do Work.....");
+        }
+
+
+    }
+
+}
